7860348972cbe6da1cc6945ae8ab9ebe413122cc
Juanpablo Barahona <juanpablo.barahonaa@gmail.com>
2017-04-07 00:20:26 -0300

    Eliminar archivos y directorios para subir los componentes del proyecto de manera ordenanda

--- src/app/generar-informe/informe.ts
+++
@@ -1,9 +0,0 @@
-export class Informe {
-    constructor(
-        public tituloInforme: string,
-        public sedeInforme: string,
-        public fechaInforme: string,
-        public objetivoGeneral: Array<string>,
-        public objEspec: Array<string>
-    ){}
-}
