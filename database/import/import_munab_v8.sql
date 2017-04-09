4b5f5df5c8c2ad11edcf394d9e17ec354fc3ae22
Juanpablo Barahona <juanpablo.barahonaa@gmail.com>
2017-04-09 15:47:07 -0300

    Añade import para proyecto, reunion, usuarioproyecto, usuarioreunion

--- database/import/import_munab_v8.sql
+++ database/import/import_munab_v8.sql
@@ -1,12 +1,28 @@
 
-LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/status.csv'
-INTO TABLE munab_v8.status
+LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/Status.csv'
+INTO TABLE munab_v8.Status
 FIELDS TERMINATED BY ';';
 
-LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/rol.csv'
-INTO TABLE munab_v8.rol
+LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/Rol.csv'
+INTO TABLE munab_v8.Rol
 FIELDS TERMINATED BY ';';
 
-LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/usuario.csv'
-INTO TABLE munab_v8.usuario
+LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/Usuario.csv'
+INTO TABLE munab_v8.Usuario
+FIELDS TERMINATED BY ';';
+
+LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/Proyecto.csv'
+INTO TABLE munab_v8.Proyecto
+FIELDS TERMINATED BY ';';
+
+LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/Reunion.csv'
+INTO TABLE munab_v8.Reunion
+FIELDS TERMINATED BY ';';
+
+LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/Usuarioproyecto.csv'
+INTO TABLE munab_v8.Usuarioproyecto
+FIELDS TERMINATED BY ';';
+
+LOAD DATA INFILE '/Users/juan-pabloandresbarahonaaravena/dev/NodeJs/MinutasOnlineV2/database/import/Usuarioreunion.csv'
+INTO TABLE munab_v8.Usuarioreunion
 FIELDS TERMINATED BY ';';
